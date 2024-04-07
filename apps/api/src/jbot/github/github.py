7b016e21bdec152504a3997b7a2de37e408f267e
Jay Nam <siisee111@gmail.com>
2024-04-07 17:06:42 +0900

    feat: search code endpoint

--- apps/api/src/jbot/github/github.py
+++ apps/api/src/jbot/github/github.py
@@ -1,3 +1,4 @@
+import json
 from jbot import config
 
 from github import Github
@@ -7,6 +8,8 @@ from github import Auth
 
 
 class MyGithub:
+    github: Github
+
     def __init__(self):
         app_id = config.get_or_error("GITHUB_APP_ID")
         private_key = config.get_or_error("GITHUB_PRIVATE_KEY")
@@ -17,4 +20,9 @@ class MyGithub:
         self.github = Github(auth=auth)
         self.repo = self.github.get_repo(config.get("GITHUB_REPO"))
         print(self.repo.name)
-        pass
+
+    def search_code(self, query: str):
+        github_search_query = f"{query} repo:{config.get('GITHUB_REPO')}"
+        search_result = self.github.search_code(github_search_query).get_page(0)
+        print(search_result[0].path, search_result[0].decoded_content)
+        return search_result[0].decoded_content
--- apps/api/src/jbot/routers/github.py
+++ apps/api/src/jbot/routers/github.py
@@ -12,3 +12,8 @@ github = MyGithub()
 @router.get("/repo-name")
 def get_repo_name():
     return github.repo.name
+
+
+@router.get("/search-code")
+def search_code(query: str):
+    return github.search_code(query)
